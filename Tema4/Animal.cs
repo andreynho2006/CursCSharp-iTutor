ba68fe73c1e35def98f017342495d2c9b3fcac6f
andreynho2006 <andreynho2006@gmail.com>
2020-02-18 16:34:01 +0200

    make Animal class

---
+++ Tema4/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Tema4
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            this.tipAnimal = "";
+            this.culoare = "";
+            this.varsta = 0;
+        }
+
+        public Animal(string tipAnimal)
+        {
+            this.tipAnimal = tipAnimal;
+            this.culoare = "";
+            this.varsta = 0;
+        }
+
+        public Animal(string tipAnimal, string culoare)
+        {
+            this.tipAnimal = tipAnimal;
+            this.culoare = culoare;
+            this.varsta = 0;
+        }
+
+        public Animal(string tipAnimal, string culoare, int varsta)
+        {
+            this.tipAnimal = tipAnimal;
+            this.culoare = culoare;
+            this.varsta = varsta;
+        }
+
+        public string tipAnimal { get; set; }
+        public string culoare { get; set; }
+        public int varsta { get; set; }
+    }
+
+    
+}
