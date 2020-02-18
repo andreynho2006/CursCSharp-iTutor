12a9f184bd9b0f5239083c3c46734a672ec9ab17
andreynho2006 <andreynho2006@gmail.com>
2020-02-18 17:02:37 +0200

    add Imobil class with 3 objects

---
+++ Tema4/Imobil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Tema4
+{
+    public class Imobil
+    {
+        public Imobil()
+        {
+            this.tipCasa = "";
+            this.nrEtaje = 0;
+            this.pret = 0.0d;
+        }
+
+        public Imobil(string tipCasa)
+        {
+            this.tipCasa = tipCasa;
+            this.nrEtaje = 0;
+            this.pret = 0.0d;
+        }
+
+        public Imobil(string tipCasa, int nrEtaje)
+        {
+            this.tipCasa = tipCasa;
+            this.nrEtaje = nrEtaje;
+            this.pret = 0.0d;
+        }
+
+        public Imobil(string tipCasa, int nrEtaje, double pret)
+        {
+            this.tipCasa = tipCasa;
+            this.nrEtaje = nrEtaje;
+            this.pret = pret;
+        }
+
+        public string tipCasa { get; set; }
+        public int nrEtaje { get; set; }
+        public double pret { get; set; }
+    }
+}
--- Tema4/Program.cs
+++ Tema4/Program.cs
@@ -9,6 +9,28 @@ namespace Tema4
             Animal cat = new Animal("cat", "red", 4);
             Animal dog = new Animal("dog", "brown");
             Animal horse = new Animal("horse");
+
+            dog.varsta = 3;
+
+            horse.culoare = "brown";
+            horse.varsta = 7;
+
+            Console.WriteLine($"Animal {cat.tipAnimal} is {cat.culoare} and has {cat.varsta} years.");
+            Console.WriteLine($"Animal {dog.tipAnimal} is {dog.culoare} and has {dog.varsta} years.");
+            Console.WriteLine($"Animal {horse.tipAnimal} is {horse.culoare} and has {horse.varsta} years.");
+
+            Imobil apartament = new Imobil("apartament", 1, 400000.00);
+            Imobil casa = new Imobil("casa", 3);
+            Imobil vila = new Imobil("vila");
+
+            casa.pret = 345000.00;
+
+            vila.nrEtaje = 2;
+            vila.pret = 79000.00;
+
+            Console.WriteLine($"Imobilul tip {apartament.tipCasa} are {apartament.nrEtaje} etaje si un pret de {apartament.pret} lei.");
+            Console.WriteLine($"Imobilul tip {casa.tipCasa} are {casa.nrEtaje} etaje si un pret de {casa.pret} lei.");
+            Console.WriteLine($"Imobilul tip {vila.tipCasa} are {vila.nrEtaje} etaje si un pret de {vila.pret} lei.");
         }
     }
 }
